b5736d342404a2fdf6068767a5d67a8987d521b1
Brett Chiu <chiubrett2500@gmail.com>
2025-02-24 16:16:01 -0800

    Space Invaders basics

--- space_invaders.py
+++ space_invaders.py
@@ -1,9 +1,21 @@
 import pygame
-
+import sys
 from ship import Ship
 
 class SpaceInvaders:
     def __init__(self):
         pygame.init()
+        self.screen = pygame.display.set_mode((1280,720))
+        pygame.display.set_caption("Space Invaders")
 
     def run_game(self):
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            pygame.display.flip()
+
+if __name__ == "__main__":
+    game = SpaceInvaders()
+    game.run_game()
