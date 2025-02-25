cbfbc24ed79fb7531437715a74e45b2a9d70bb2c
Brett Chiu <chiubrett2500@gmail.com>
2025-02-24 16:26:05 -0800

    added files

---
+++ aliens.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.sprite import Sprite
+
+# class Aliens(Sprite):
+#     def __init__(self, screen, x, y):
+#         pass
+
+# class PinkAlien(Aliens):
+
+# class BlueAlien(Aliens):
+
+# class GreenAlien(Aliens):
+
+# class RedAlien(Aliens):
+#     def __init__(self, screen, x, y):
+#         pass
--- space_invaders.py
+++ space_invaders.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from ship import Ship
+from aliens import Aliens
 
 class SpaceInvaders:
     def __init__(self):
