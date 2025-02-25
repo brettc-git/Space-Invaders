d06f0edd24c44513e48bb6925d303f9872809f71
Brett Chiu <chiubrett2500@gmail.com>
2025-02-24 16:26:26 -0800

    Settings

---
+++ settings.py
@@ -0,0 +1,12 @@
+# COLORS
+
+GREEN = (0,255,0)
+RED = (255,0,0)
+CYAN = (0,255,255)
+PINK = (255, 0, 255)
+BLACK = (0,0,0)
+class Settings:
+    def __init__(self):
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.bg_color = BLACK
