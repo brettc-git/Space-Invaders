0fa207937432f75f9c05f897aea85b1bbcbe32e3
Brett Chiu <chiubrett2500@gmail.com>
2025-02-24 20:05:58 -0800

    alien inheritance

--- aliens.py
+++ aliens.py
@@ -5,12 +5,16 @@ class Aliens(Sprite):
     def __init__(self, screen, x, y):
         pass
 
-# class PinkAlien(Aliens):
+class PinkAlien(Aliens):
+    def __init__(self, screen, x, y):
+        pass
+class BlueAlien(Aliens):
+    def __init__(self, screen, x, y):
+        pass
+class GreenAlien(Aliens):
+    def __init__(self, screen, x, y):
+        pass
 
-# class BlueAlien(Aliens):
-
-# class GreenAlien(Aliens):
-
-# class RedAlien(Aliens):
-#     def __init__(self, screen, x, y):
-#         pass
+class RedAlien(Aliens):
+    def __init__(self, screen, x, y):
+        pass
