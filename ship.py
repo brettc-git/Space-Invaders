af14a458873cb8f286ff4d06a742e306841594d2
Brett Chiu <chiubrett2500@gmail.com>
2025-03-01 23:47:59 -0800

    Ship _init_

--- ship.py
+++ ship.py
@@ -2,11 +2,27 @@ import pygame
 from pygame.sprite import Sprite
 
 class Ship(Sprite):
-    def __init__(self):
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
 
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x) # Store exact position of ship horizontal float
 
         self.moving_left = False
         self.moving_right = False
+
+    def update(self):
+        # Update ship movement
+
+        self.rect.x = self.x
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
--- space_invaders.py
+++ space_invaders.py
@@ -8,12 +8,11 @@ from settings import Settings
 class SpaceInvaders:
     def __init__(self):
         pygame.init()
+        self.clock = pygame.time.Clock()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((1280,720))
         pygame.display.set_caption("Space Invaders")
-
-        self.bg_color = self.settings.bg_color
-
+        self.ship = Ship(self)
 
         self.game_active = False
 
@@ -24,6 +23,16 @@ class SpaceInvaders:
                 if event.type == pygame.QUIT:
                     sys.exit()
             pygame.display.flip()
+            self.clock.tick(60)
+
+    def center_ship(self):
+        # Center the ship on the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.ship.x = float(self.rect.x)
+
+    def update_screen(self):
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
 
 if __name__ == "__main__":
     game = SpaceInvaders()
